52171bb2955a0b22ae3c286eedabf4c56697f17b
Jonas Hampel <98750204+Deviljho35@users.noreply.github.com>
2023-11-02 12:10:54 +0100

    Update HamsterErweitert.java
    Constructor Added

--- HamsterErweitert.java
+++ HamsterErweitert.java
@@ -1,6 +1,9 @@
 import de.hamster.debugger.model.Territorium;import de.hamster.debugger.model.Territory;import de.hamster.model.HamsterException;import de.hamster.model.HamsterInitialisierungsException;import de.hamster.model.HamsterNichtInitialisiertException;import de.hamster.model.KachelLeerException;import de.hamster.model.MauerDaException;import de.hamster.model.MaulLeerException;import de.hamster.model.MouthEmptyException;import de.hamster.model.WallInFrontException;import de.hamster.model.TileEmptyException;import de.hamster.debugger.model.Hamster;public class HamsterErweitert implements de.hamster.model.HamsterProgram {
 class HamsterV2 extends Hamster{
 
+    HamsterV2(){
+        
+    }
     void kehrt(){
         linksUm();
         linksUm();
