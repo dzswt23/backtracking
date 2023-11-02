75cefa5f8cd5bfd4f7171dbdc90add955fb7999d
Jonas Hampel <98750204+Deviljho35@users.noreply.github.com>
2023-11-02 11:58:12 +0100

    Create HamsterErweitert.java
    Created the class HamsterErweitert.java with methods: "kehrt()", "rechtsUm()" and "laufeBisZurWand()".

---
+++ HamsterErweitert.java
@@ -0,0 +1,22 @@
+import de.hamster.debugger.model.Territorium;import de.hamster.debugger.model.Territory;import de.hamster.model.HamsterException;import de.hamster.model.HamsterInitialisierungsException;import de.hamster.model.HamsterNichtInitialisiertException;import de.hamster.model.KachelLeerException;import de.hamster.model.MauerDaException;import de.hamster.model.MaulLeerException;import de.hamster.model.MouthEmptyException;import de.hamster.model.WallInFrontException;import de.hamster.model.TileEmptyException;import de.hamster.debugger.model.Hamster;public class HamsterErweitert implements de.hamster.model.HamsterProgram {class HamsterV2 extends Hamster{
+
+void kehrt(){
+linksUm();
+linksUm();
+}
+void rechtsUm(){
+kehrt();
+linksUm();
+}
+void laufeBisZurWand(){
+while(vornFrei()){
+vor();
+} 
+}
+}
+public void main() {
+HamsterV2 h = new HamsterV2();
+h.init(0,0,Hamster.OST, 0);
+h.laufeBisZurWand();    
+}
+}
